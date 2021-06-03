d3b4e399197d6ed3fbbef0b0eea3435908a27bee
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-06-02 21:13:38 -0400

    common filename

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 set -x
-mysqldump -u ${MARIADB_USER} -p${MARIADB_PASSWORD} --all-databases -h${HOST} -P${PORT} | lz4 -c | ssh -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} " cat > ${TARGET_PATH}"
+EPOCH=$(date +%s)
+HOSTNAME=$(hostname)
+FILENAME="${HOSTNAME}-${EPOCH}.sql.lz4"
+mysqldump -u ${MARIADB_USER} -p${MARIADB_PASSWORD} --all-databases -h${HOST} -P${PORT} | lz4 -c | ssh -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST} " cat > ${TARGET_PATH}/${FILENAME}"
